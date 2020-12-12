3f9284396008f3f8503ac7c2e743166c711b7231
hongyeon <hyjykelly@gmail.com>
2020-12-12 06:30:27 +0000

    20201212

--- Implement/11.py
+++ Implement/11.py
@@ -1,70 +1,60 @@
+def build_stick(sticks, bos ,x,y) :
+    if y == 0 or sticks[x][y-1] : 
+        return True
+    if x>0 and bos[x-1][y] or bos[x][y] :	
+        return True
+    return False
 
+def build_bo(sticks, bos ,x,y,n) :
+    #보의 한쪽 끝 부분이 기둥 위에 있거나
+    if sticks[x][y-1] or sticks[x+1][y-1] :
+        return True
+    # 양쪽 끝부분이 다른 보와 동시에 연결되어 있는 경우
+    if x > 0 and bos[x-1][y] and bos[x+1][y] :
+        return True
+    return False
 
-build_frame = [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
-n = 5
-cnt = len(build_frame)
+def remove(sticks, bos,x, y,n) :
+    for i in range(n+1) :
+        for j in range(n+1) :
+            if sticks[i][j] :
+                if not build_stick(sticks, bos,i, j) :
+                    return False
+            if bos[i][j] :
+                if not build_bo(sticks, bos ,i,j,n) :
+                    return False
+    return True
 
-maps = [[2 for _ in range(n+1)] for _ in range(n+1)]
+def solution(n, build_frame):
 
-def build_stick(x,y) :
-	if y == 0 :     # 바닥에 있거나
-		return True
-	if maps[x][y-1] == 0 : # 다른 기둥 위에 있거나 
-			return True
-	if x-1 >= 0 :
-		if maps[x-1][y] == 1 :  # 보의 한쪽 끝 부분에 있거나 
-			return True
-	if maps[x][y] == 1 :				# 보의 한쪽 끝 부분에 있거나 
-		return True
-	return False
+    sticks = [[0] * (n+1) for i in range(n+1)]
+    bos = [[0] * (n+1) for i in range(n+1)]
+    answer = []
 
-	
-def build_bo(x,y) :
-	#보의 한쪽 끝 부분이 기둥 위에 있거나
-	if maps[x][y-1] == 0 :
-			return True
-	if maps[x+1][y-1] == 0 :
-			return True
-	# 양쪽 끝부분이 다른 보와 동시에 연결되어 있는 경우
-	if x-1 >=0 and x+1 <= n :
-		if maps[x-1][y] == 1 and maps[x+1][y] == 1 :
-			return True
-	return False
+    for x, y, a, b in build_frame :
+        if b == 1 : 
+            if a == 0 :
+                if build_stick(sticks, bos, x,y) :
+                    sticks[x][y] = 1
+            elif a == 1 : 
+                if build_bo(sticks, bos, x,y,n) :
+                    bos[x][y] = 1
+        elif b == 0 :
+            if a == 0 :
+                sticks[x][y] = 0
+            elif a == 1 :
+                bos[x][y] = 0
+            if not remove(sticks, bos,x,y,n) :
+                if a == 0:
+                    sticks[x][y] = 1
+                else :
+                    bos[x][y] = 1
+                    
+    for i in range(len(sticks[0])) :
+        for j in range(len(sticks))	:
+            if sticks[i][j] == 1 :
+                answer.append([i,j,0])
+            if bos[i][j] == 1 :
+                answer.append([i,j,1])
 
-def remove(x, y) :
-	for i in range(n+1) :
-		for j in range(n+1) :
-			if maps[i][j] == 0 :
-				if build_stick(i, j) == False:
-					return False
-			elif maps[i][j] == 1 :
-				if build_bo(i,j) == False :
-					return False
-	return True
-
-for x, y, a, b in build_frame :
-	if b == 1 : 
-		if a == 0 :
-			if build_stick(x,y) :
-				maps[x][y] = 0
-		else : 
-			if build_bo(x,y) :
-				maps[x][y] = 1
-	else :
-		temp = maps[x][y]
-		maps[x][y] = 2
-		if remove(x,y) == False :
-			maps[x][y] = temp
-
-answer = list()
-for i in range(n+1) :
-	for j in range(n+1)	:
-		res = list()
-		if maps[i][j] == 0 or maps[i][j] == 1 :
-			res.append(i)
-			res.append(j)
-			res.append(maps[i][j])
-			answer.append(res)
-
-print(answer)
-
+    return answer
