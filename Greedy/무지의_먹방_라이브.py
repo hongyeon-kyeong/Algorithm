6d66ad95849639d4b23037c360e745f1440ea54a
hongyeon <hyjykelly@gmail.com>
2020-12-29 12:59:58 +0000

    20201229

--- Greedy/무지의_먹방_라이브.py
+++ Greedy/무지의_먹방_라이브.py
@@ -5,6 +5,7 @@ IDEA
 - 멈췄는데 나머지가 있는 경우 해당 인덱스 리턴
 '''
 
+'''
 def solution(food_times, k):
 	M = len(food_times)
 	while True :
@@ -42,4 +43,59 @@ def solution(food_times, k):
 				return i+2
 	return -1
 
-print(solution([3, 1, 2],5))
+def solution(food_times, k) :
+	N = len(food_times)
+	i = 0
+	for _ in range(k) :
+		while True : 
+			if food_times[i] != 0 :
+				food_times[i] -= 1
+				print('i', i, food_times[i])
+				i += 1
+				break
+			else :
+				print('i', i, food_times[i])
+				i += 1
+				if i == N : i = 0
+		if i == N : i = 0
+	while True :
+		if food_times[i+1] == 0 :
+			i +=1
+			if i+1 == N : i = -1
+			print('i+1', i+1, food_times[i+1])
+		else : 
+			return i+2
+	return -1
+
+print(solution([3, 1, 2,3,7,5],23))
+'''
+def find_food(food_times,i,n) :
+	count2 = 0
+	if i == n :
+		i = 0
+	
+	while food_times[i] == 0 :
+		i += 1
+		count2 += 1
+		#print('i', i , 'count2', count2)
+		if count2 == n :
+			return -2
+		if i == n :
+			i = 0
+	return i
+
+def solution(food_times, k) :
+	food = -1
+	count = 0
+	n = len(food_times)
+	while count < k :
+		food = find_food(food_times, food+1, n)
+		if food == -2 :
+			return -1
+		food_times[food] -= 1  # 먹었다.
+		count += 1 # 1초가 지났다.
+		print(count, food_times, food)
+	res = find_food(food_times, food+1,n)
+	return res+1
+
+print(solution([7,8,3,3,2,2,2,2,2,2,2,2],35))
