6ae49e9bb4ac9fe07d8a568bdb402170306ffbf1
hongyeon <hyjykelly@gmail.com>
2021-01-20 13:57:46 +0000

    20210120

--- Dynamic_Programming/33.py
+++ Dynamic_Programming/33.py
@@ -7,14 +7,53 @@ pay = []
 for _ in range(n) :
 	t, p = map(int, input().split())
 	time.append(t)
-	pay.append(t)
+	pay.append(p)
 
 def func(d) :
 	if d + time[d] > n :
 		pay[d] = 0
+	res = 0
 	for i in range(d):
 		if i + time[i] <= d :
-			pay[d] = max(pay[d], func(i))
+			res = max(res, func(i))
+	pay[d] += res
+	print(d, pay)
 	return pay[d]
 
-print(func(n-1))
+for i in range(n) :
+	if i + time[i] > n :
+		pay[i] = 0
+	res = 0
+	for j in range(i) :
+		if j + time[j] <= i :
+			res = max(res, pay[j])
+	pay[i] += res
+	#print(i, pay)
+
+print(max(pay))
+'''
+7
+3 10
+5 20
+1 10
+1 20
+2 15
+4 40
+2 200
+45
+
+10
+1 1
+1 2
+1 3
+1 4
+1 5
+1 6
+1 7
+1 8
+1 9
+1 10
+55
+
+
+'''
