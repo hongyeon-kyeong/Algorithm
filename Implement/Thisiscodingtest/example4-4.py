d4a5f2b02247b788ca202646295a904d1846b351
hongyeon <hyjykelly@gmail.com>
2021-02-04 12:33:22 +0000

    20210204

--- Implement/Thisiscodingtest/example4-4.py
+++ Implement/Thisiscodingtest/example4-4.py
@@ -1,5 +1,7 @@
 n, m = map(int, input().split())
 a, b, d = map(int, input().split())
+
+# 방문여부를 기록
 data = [[0]*m for _ in range(n)]
 array = []
 for _ in range(n) :
@@ -9,41 +11,39 @@ dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 data[a][b] = 1
 
-def rotate(d) :
+def rotate() :
+	global d
 	d -= 1
 	if d == -1 :
-		d == 3
-		
-count = 0
-def move(a, b) :
+		d = 3
+
+count = 1
+turn_count = 0
+while True :
 	
-	global count
-	for _ in range(4) :
-		na = a + dx[d]
-		nb = b + dy[d]
-		print(na,nb)
+	rotate()
+	nx = a + dx[d]
+	ny = b + dy[d]
 
-		if na < 0 or nb < 0 or na >= n or nb >= m :
-			return
-		
-		if array[na][nb] == 0 :
-			array[na][nb] = 2
-			count += 1
-			return move(na, nb)
-		else :
-			rotate(d)
-	
-	na = a + dx[d]*-1
-	nb = b + dy[d]*-1
-
-	if na < 0 or nb < 0 or na >= n or nb >= m :
-		return
-
-	if array[na][nb] == -1 :
-		return
+	if array[nx][ny] == 0 and data[nx][ny] == 0 :
+		data[nx][ny] = 1
+		turn_count = 0
+		a, b = nx, ny
+		count += 1
+		continue
 	else :
-		return move(na,nb)
+		turn_count +=1
 
-move(a,b)
+		if turn_count == 4 :
+			nx = a + dx[d]*-1
+			ny = a + dy[d]*-1
+
+			if array[nx][ny] != 1 :
+				a, b = nx, ny
+			else :
+				break
+			turn_count = 0
+
 print(count)
+		
 
