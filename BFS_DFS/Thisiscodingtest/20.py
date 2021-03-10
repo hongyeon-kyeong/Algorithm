9e03a74570404bbd95aec78210a81d1811e46e72
hongyeon <59414678+hongyeon-kyeong@users.noreply.github.com>
2021-03-11 07:33:31 +0900

    Update 20.py

--- BFS_DFS/Thisiscodingtest/20.py
+++ BFS_DFS/Thisiscodingtest/20.py
@@ -1,67 +1,57 @@
-from itertools import combinations
 n = int(input())
-board = []
-teachers = []
-spaces = []
+graph = []
+tmp_graph = []
+for _ in range(n) :
+	graph.append(list(input().split()))
 
 for i in range(n) :
-	data = list(input().split())
-	board.append(data)
-	for j in range(n) :
-		if data[j] == 'T' :
-			teachers.append((i, j))
-		if data[j] == 'X' :
-			spaces.append((i,j))
+	tmp_graph.append(graph[i])
 
-def watch(x,y,direction) :
-	if direction == 0 :
-		while y >= 0 :
-			if board[x][y]  == 'S' :
-				return True
-			if board[x][y] == 'O' :
-				return False
-			y -= 1
-	elif direction == 1 :
-		while y < n :
-			if board[x][y]  == 'S' :
-				return True
-			if board[x][y] == 'O' :
-				return False
-			y += 1
-	elif direction == 2 :
-		while x >= 0 :
-			if board[x][y]  == 'S' :
-				return True
-			if board[x][y] == 'O' :
-				return False
-			x -= 1
-	else :
-		while x < n :
-			if board[x][y]  == 'S' :
-				return True
-			if board[x][y] == 'O' :
-				return False
-			x += 1
+def detect(a, b) :
+	da = [-1,1,0,0]
+	db = [0,0,-1,1]
 
-def process() :
-	for x, y in teachers :
-		for i in range(4) :
-			if watch(x, y, i) :
-				return True
+	for d in range(4) :
+		na = a + da[d]
+		nb = b + db[d]
+
+		while True :
+			if na >= 0 and nb >= 0 and na < n and nb < n :
+				if tmp_graph[na][nb] != 'O' :
+					if tmp_graph[na][nb] == 'S' :
+						return True
+					else :
+						na += da[d]
+						nb += db[d]
+				else :
+					break
+			else :
+				break
 	return False
 
-find = False
-for space in combinations(spaces, 3) :
-	for x, y in space :
-		board[x][y] = 'O'
-	if not process() :
-		find = True
-		break
+chk = False
+def dfs(count) :
+	global chk
+	if count == 3 :
+		detect_yn = False
+		for i in range(n) :
+			for j in range(n) :
+				if tmp_graph[i][j] == 'T' :
+					if detect(i, j) :
+						detect_yn = True
+		if not detect_yn :
+			chk = True
 	else :
-		for x, y in space :
-			board[x][y] = 'X'
-
-if find : 
+		for i in range(n) :
+			for j in range(n) :
+				if tmp_graph[i][j] == 'X' :
+					count += 1
+					tmp_graph[i][j] = 'O'
+					dfs(count)
+					count -= 1
+					tmp_graph[i][j] = 'X'
+dfs(0)
+if chk :
 	print('YES')
 else :
 	print('NO')
